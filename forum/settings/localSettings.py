97c2391733f4ac14a9c6278640e5c90c09f29c76
wilondja243 <lucksonpremier@gmail.com>
2025-03-10 02:37:55 +0200

    Update forum project

---
+++ forum/settings/localSettings.py
@@ -0,0 +1,11 @@
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'forum',
+        'PASSWORD': 'luckson243',
+        'USER': 'luckson',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
